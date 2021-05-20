87478974e56a69cc23124c4dcc2366f1fed488f6
Jordan Yates <jordan.yates@data61.csiro.au>
2021-05-20 11:12:42 +1000

    lora: sx126x: run interrupts on separate workq
    Instantiate a new workqueue for handling interrupts from the modem.
    Interrupts can trigger significant processing, including power
    transitions on multiple devices. If run on the system workqueue, this
    can cause degraded performance and potentially leads to deadlocks.

--- drivers/lora/sx126x.c
+++ drivers/lora/sx126x.c
@@ -65,6 +65,9 @@ BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(semtech_sx1261) +
 
 #define SX126X_CALIBRATION_ALL 0x7f
 
+K_KERNEL_STACK_DEFINE(sx126x_work_q_stack, 1024);
+struct k_work_q sx126x_work_q;
+
 struct sx126x_data {
 	const struct device *reset;
 	const struct device *busy;
@@ -473,7 +476,7 @@ static void sx126x_dio1_irq_callback(const struct device *dev,
 				     struct gpio_callback *cb, uint32_t pins)
 {
 	if (pins & BIT(GPIO_DIO1_PIN)) {
-		k_work_submit(&dev_data.dio1_irq_work);
+		k_work_submit_to_queue(&sx126x_work_q, &dev_data.dio1_irq_work);
 	}
 }
 
@@ -491,6 +494,15 @@ static int sx126x_lora_init(const struct device *dev)
 	    sx12xx_configure_pin(tx_enable, GPIO_OUTPUT_INACTIVE)) {
 		return -EIO;
 	}
+
+	const struct k_work_queue_config q_cfg = {
+		.name = "sx1262_q"
+	};
+	k_work_queue_start(&sx126x_work_q,
+			   sx126x_work_q_stack,
+			   K_KERNEL_STACK_SIZEOF(sx126x_work_q_stack),
+			   -1,
+			   &q_cfg);
 
 	k_work_init(&dev_data.dio1_irq_work, sx126x_dio1_irq_work_handler);
 
